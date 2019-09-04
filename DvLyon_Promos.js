0311145f62b47017435bd2185fa277055b9eaf2f
David Lyon <davilyon1988@gmail.com>
2019-09-04 18:51:13 -0400

    Updated versions with kks and upload promos v1

--- DvLyon_Promos.js
+++ DvLyon_Promos.js
@@ -1,31 +1,31 @@
 //=============================================================================
-// Musubi Game Studio
-// Musubi_Splash.js
+// DvLyon Games
+// DvLyon_Promos.js
 //=============================================================================
 
 var Imported = Imported || {};
-Imported.Musubi_Splash = true;
+Imported.DvLyon_Promos = true;
 
-var Musubi = Musubi || {};
-Musubi.Splash = Musubi.Splash || {};
-Musubi.Splash.version = 1.21;
+var DvLyon = DvLyon || {};
+DvLyon.Promos = DvLyon.Promos || {};
+DvLyon.Promos.version = 1;
 
 /*:
 -------------------------------------------------------------------------
-@title Musubi Splash
-@author Musubi @ https://gamestudio.musubiapp.com
-@date Apr 29, 2019
-@version 1.2.1
-@filename Musubi_Splash.js
-@url https://gamestudio.musubiapp.com
+@title DvLyon Promos
+@author DvLyon Games @ https://games.dvlyon.com
+@date Sep 4, 2019
+@version 1.0.0
+@filename DvLyon_Promos.js
+@url https://games.dvlyon.com
 
 Contact:
 
-* Main Website: https://gamestudio.musubiapp.com
-* Twitter: https://twitter.com/MusubiTeam
+* Website: https://games.dvlyon.com
+* Twitter: https://twitter.com/DvLyon
 
 -------------------------------------------------------------------------------
-@plugindesc Musubi Splash Screen
+@plugindesc DvLyon Boot Promos
 @help 
 -------------------------------------------------------------------------------
 == Description ==
@@ -40,24 +40,14 @@ Displays an image on game load, before Title Screen.
 
 == Terms of Use ==
 
-If you could credit Musubi and https://gamestudio.musubiapp.com, we'd really
+If you could credit DvLyon and https://games.dvlyon.com, we'd really
 appreciate it!
 
 We want to keep growing and making your RMMV experience better!
 
 == Change Log ==
 
-1.2.1 - Apr 29, 2019
- * (QoL) Plugin's description typo fixed.
-1.2.0 - Mar 31, 2019
- * (Feature) Compatibility with Musubi_Core version 2.1.0.
- * (Feature) Added option to set fade speed.
- * (Optimization) Reworked the Splash Screen update method.
-1.1.0 - Mar 29, 2019
- * Added ability to choose the image to be displayed.
- * Added Musubi_Core dependency.
- * Upgraded plugin layout.
-1.0.0 - Feb 19, 2019
+1.0.0 - Sep 4, 2019
  * Release.
 
 == Usage ==
@@ -66,14 +56,14 @@ Install, activate and configure parameters.
 
 == Notes ==
 
-Images must be placed in img/musubi.
+Images must be placed in img/dvlyon.
 
 -------------------------------------------------------------------------------
  *
  * @param Image
  * @desc Image to display on game load.
  * @type file
- * @dir img/musubi/
+ * @dir img/dvlyon/
  * @require 1
  * @default
  *
@@ -88,7 +78,7 @@ Images must be placed in img/musubi.
 // Dependencies
 //=============================================================================
 
-if (Imported.Musubi_Core && Musubi.Core && Musubi.Core.version >= 2.1) {
+if (Imported.DvLyon_Core && DvLyon.Core && DvLyon.Core.version >= 1) {
 
 //=============================================================================
 // Plugin Start
@@ -98,53 +88,53 @@ if (Imported.Musubi_Core && Musubi.Core && Musubi.Core.version >= 2.1) {
 
 	/* Parameters */
 
-	Musubi.Splash.Parameters = PluginManager.parameters('Musubi_Splash')
+	DvLyon.Promos.Parameters = PluginManager.parameters('DvLyon_Promos')
 
-	Musubi.Splash.Image = Musubi.Splash.Parameters['Image']
-	Musubi.Splash.FadeSpeed = toNumber(Musubi.Splash.Parameters['FadeSpeed'], 24)
+	DvLyon.Promos.Image = DvLyon.Promos.Parameters['Image']
+	DvLyon.Promos.FadeSpeed = toNumber(DvLyon.Promos.Parameters['FadeSpeed'], 24)
 
 	/* Scene_Boot */
 
 	var _Scene_Boot_loadSystemImages = Scene_Boot.prototype.loadSystemImages
 	Scene_Boot.prototype.loadSystemImages = function() {
 		_Scene_Boot_loadSystemImages.call(this)
-		ImageManager.loadMusubi(Musubi.Splash.Image)
+		ImageManager.loadDvLyon(DvLyon.Promos.Image)
 	}
 
 	var _Scene_Boot_start = Scene_Boot.prototype.start
 	Scene_Boot.prototype.start = function() {
 		if (!DataManager.isBattleTest() && !DataManager.isEventTest()) {
-			SceneManager.goto(Scene_MusubiSplash)
+			SceneManager.goto(Scene_DvLyonPromos)
 		} else {
 			_Scene_Boot_start.call(this)
 		}
 	}
 
 	//-----------------------------------------------------------------------------
-	// Scene_MusubiSplash
+	// Scene_DvLyonPromos
 	//
-	// The scene class for showing the Musubi Game Studio splash screen.
+	// The scene class for showing the DvLyon Games splash screen.
 
-	function Scene_MusubiSplash() {
+	function Scene_DvLyonPromos() {
 	    this.initialize.apply(this, arguments)
 	}
 
-	Scene_MusubiSplash.prototype = Object.create(Scene_Base.prototype);
-	Scene_MusubiSplash.prototype.constructor = Scene_MusubiSplash;
+	Scene_DvLyonPromos.prototype = Object.create(Scene_Base.prototype)
+	Scene_DvLyonPromos.prototype.constructor = Scene_DvLyonPromos
 
-	Scene_MusubiSplash.prototype.initialize = function() {
+	Scene_DvLyonPromos.prototype.initialize = function() {
 		Scene_Base.prototype.initialize.call(this)
 		this._splashImage = null
 		this._splashFadeIn = false
 		this._splashFadeOut = false
 	}
 
-	Scene_MusubiSplash.prototype.create = function() {
+	Scene_DvLyonPromos.prototype.create = function() {
 		Scene_Base.prototype.create.call(this)
 		this.createSplashes()
 	}
 
-	Scene_MusubiSplash.prototype.start = function() {
+	Scene_DvLyonPromos.prototype.start = function() {
 		Scene_Base.prototype.start.call(this)
 		SceneManager.clearStack()
 		if (this._splashImage !== null) {
@@ -152,7 +142,7 @@ if (Imported.Musubi_Core && Musubi.Core && Musubi.Core.version >= 2.1) {
 		}
 	}
 
-	Scene_MusubiSplash.prototype.update = function() {
+	Scene_DvLyonPromos.prototype.update = function() {
 		Scene_Base.prototype.update.call(this)
 		if (!this.isBusy()) {
 			if (!this._splashFadeIn) {
@@ -167,30 +157,30 @@ if (Imported.Musubi_Core && Musubi.Core && Musubi.Core.version >= 2.1) {
 		}
 	}
 
-	Scene_MusubiSplash.prototype.createSplashes = function() {
-		this._splashImage = new Sprite(ImageManager.loadMusubi('MGSRMMV'))
+	Scene_DvLyonPromos.prototype.createSplashes = function() {
+		this._splashImage = new Sprite(ImageManager.loadDvLyon(DvLyon.Promos.Image))
 		this.addChild(this._splashImage)
 	}
 
-	Scene_MusubiSplash.prototype.centerSprite = function(sprite) {
+	Scene_DvLyonPromos.prototype.centerSprite = function(sprite) {
 		sprite.x = Graphics.width / 2
 		sprite.y = Graphics.height / 2
 		sprite.anchor.x = 0.5
 		sprite.anchor.y = 0.5
 	}
 
-	Scene_MusubiSplash.prototype.checkPlayerLocation = function() {
+	Scene_DvLyonPromos.prototype.checkPlayerLocation = function() {
 		if ($dataSystem.startMapId === 0) {
 			throw new Error('Player\'s starting position is not set')
 		}
 	}
 
-	Scene_MusubiSplash.prototype.updateDocumentTitle = function() {
+	Scene_DvLyonPromos.prototype.updateDocumentTitle = function() {
 		document.title = $dataSystem.gameTitle
 	}
 
-	Scene_MusubiSplash.prototype.fadeSpeed = function() {
-		return Musubi.Splash.FadeSpeed
+	Scene_DvLyonPromos.prototype.fadeSpeed = function() {
+		return DvLyon.Promos.FadeSpeed
 	}
 
 })()
@@ -200,7 +190,28 @@ if (Imported.Musubi_Core && Musubi.Core && Musubi.Core.version >= 2.1) {
 //=============================================================================
 
 } else {
-var text = 'Musubi_Splash requires Musubi_Core at the latest version to run.'
-console.error(text)
-require('nw.gui').Window.get().showDevTools()
+	var text = 'DvLyon_Promos requires DvLyon_Core at the latest version to run.'
+	console.error(text)
+	require('nw.gui').Window.get().showDevTools()
 }
+
+//=============================================================================
+// Version Checker
+//=============================================================================
+
+function versionChecker() {
+	var url = "https://raw.githubusercontent.com/dvlyon/RMMV-Free/master/versions.json"
+	var request = new Request(url)
+	fetch(request)
+	.then(function(response) {
+		return response.json()
+	})
+	.then(function(body) {
+		if (body && (body.promos > DvLyon.Promos.version)) {
+			var text = 'An updated version of DvLyon_Promos is available at https://games.dvlyon.com'
+			console.info(text)
+		}
+	})
+}
+
+versionChecker()
